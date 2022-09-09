a04d55355ed7a0ff55f57913313ecb60b6a6c867
Ramses Garate <47763122+ramsesgarate@users.noreply.github.com>
2022-09-09 18:49:57 -0300

    feta(format): add config for generate rut with dash

--- src/format/index.test.ts
+++ src/format/index.test.ts
@@ -7,4 +7,10 @@ describe('Format', () => {
     expect(format('6045003k')).toEqual('6.045.003-K')
     expect(format('9264930')).toEqual('926.493-0')
   })
+
+  it('Format rut dash alone', () => {
+    expect(format('125335667', { dashAlone: true })).toEqual('12533566-7')
+    expect(format('6045003k', { dashAlone: true })).toEqual('6045003-K')
+    expect(format('9264930', { dashAlone: true })).toEqual('926493-0')
+  })
 })
--- src/format/index.ts
+++ src/format/index.ts
@@ -1,5 +1,28 @@
-export function format (rut: string): string {
-  const rutFormat = rut.length > 7 ? '$1.$2.$3-$4' : '$2.$3-$4'
+export interface FormatConfig {
+  dashAlone: boolean;
+}
 
-  return rut.replace(/^(\d{1,2})?(\d{3})(\d{3})(\w{1})$/, rutFormat).toUpperCase()
+const formatType = {
+  short: { rutFormat: '$1.$2-$3', regexFormat: /^(\d{3})(\d{3})(\w{1})$/ },
+  long: { rutFormat: '$1.$2.$3-$4', regexFormat: /^(\d{1,2})?(\d{3})(\d{3})(\w{1})$/ },
+  shortSingle: { rutFormat: '$1-$2', regexFormat: /^(\d{6})(\w{1})$/ },
+  longSingle: { rutFormat: '$1-$2', regexFormat: /^(\d{7,8})(\w{1})$/ }
+}
+
+const getFormatType = (rut, dashAlone) => {
+  const typeLength = rut.length > 7 ? 'long' : 'short'
+  const isSingle = dashAlone ? 'Single' : ''
+
+  return formatType[`${typeLength}${isSingle}`]
+}
+export function format (
+  rut: string,
+  config: FormatConfig = { dashAlone: false }
+): string {
+  const { dashAlone } = config
+  const { regexFormat, rutFormat } = getFormatType(rut, dashAlone)
+
+  return rut
+    .replace(regexFormat, rutFormat)
+    .toUpperCase()
 }
